bcf33ea26d7bb16a2541c64f81c3a3e70a1e71a6
User <User@DESKTOP-D6OVAIF>
2018-11-22 18:09:42 -0500

    hasta el fondo

--- HellishRoad(ProyectoFinalAPOII)/src/modelo/Juego.java
+++ HellishRoad(ProyectoFinalAPOII)/src/modelo/Juego.java
@@ -1,8 +1,15 @@
 package modelo;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.security.SecureRandom;
+import java.util.ArrayList;
 
-import excepciones.JugadorNoEncontradoException;
+import javax.crypto.spec.IvParameterSpec;
+
 import excepciones.PuntajeNoExisteException;
 
 public class Juego {
@@ -12,9 +19,10 @@ public class Juego {
 	private CaraJugador jugador;
 	private Jugador usuario;
 	private int tamanio;
+	private ArrayList<Jugador> users;
 	
 	public Juego() {
-		
+		users = new ArrayList<Jugador>();
 	}
 
 	public Background getBackground() {
@@ -61,7 +69,7 @@ public class Juego {
 
 		CaraAdversario busta = new CaraAdversario("data/busta.jpeg", 300, 12, 100, 100, 5);
 		CaraAdversario frapi = new CaraAdversario("data/frapi.jpeg", 400, 200, 100, 100, 5);
-		CaraAdversario yaker = new CaraAdversario("data/jackTheRipper.jpeg", 450, 62, 100, 100, 5);
+		CaraAdversario yaker = new CaraAdversario("data/jackTheRipper.jpeg", 500, 62, 100, 100, 5);
 		
 		crearListaEnemigos(busta);
 		crearListaEnemigos(frapi);
@@ -89,19 +97,22 @@ public class Juego {
 	
 	public void resetEnemigos() {
 		SecureRandom random = new SecureRandom();
-		int randomEnemigo = random.nextInt(330);
+		int randomPosY = random.nextInt(330);
 		
 		if (get(0).getPosX()<0) {
 			get(0).setPosX(700);
-			get(0).setPosY(randomEnemigo);
+			get(0).setPosY(randomPosY);
+			get(0).setImagen(enemigoRadom().getImagen());
 		}
 		if (get(1).getPosX()<0) {
 			get(1).setPosX(700);
-			get(1).setPosY(randomEnemigo);
+			get(1).setPosY(randomPosY);
+			get(1).setImagen(enemigoRadom().getImagen());
 		}
 		if (get(2).getPosX()<0) {
 			get(2).setPosX(700);
-			get(2).setPosY(randomEnemigo);
+			get(2).setPosY(randomPosY);
+			get(2).setImagen(enemigoRadom().getImagen());
 		}
 	}
 	
@@ -146,7 +157,66 @@ public class Juego {
 				}
 			}
 		}
-	}
+	} 
+	
+//	public void listaUsuarios() {
+//        if (usuario.getIzq()!=null) {
+//			usuario.getIzq().;
+//		}
+//        acumulado.add(nombre);
+//        
+//        if (usuario.getDer()!=null) {
+//			usuario.getIzq().inorden(acumulado);
+//		}
+//	}
+//	
+//	public void guardar() {
+//		FileOutputStream fS = null;
+//		ObjectOutputStream oS = null;
+//		try {
+//			fS = new FileOutputStream("src/data/Ranking.ser");
+//			oS = new ObjectOutputStream(fS);
+//			oS.writeObject(usuario);
+//		} catch (IOException ex) {
+//			System.out.println(ex.getMessage());
+//		} finally {
+//			try {
+//				if (usuario != null) {
+//					fS.close();
+//				}
+//				if (oS != null) {
+//					oS.close();
+//				}
+//			} catch (IOException ex) {
+//				System.out.println(ex.getMessage());
+//			}
+//		}
+//	}
+//	
+//	public void recuperarData() {
+//		FileInputStream fS = null;
+//		ObjectInputStream oS = null;
+//		ArrayList<Jugador> users = null;
+//		try {
+//			fS = new FileInputStream("src/data/Ranking.ser");
+//			oS = new ObjectInputStream(fS);
+//			users = (ArrayList<Jugador>) oS.readObject();
+////			setJugadores(users);
+//		} catch (Exception ex) {
+//			System.out.println(ex.getMessage());
+//		} finally {
+//			try {
+//				if (fS != null) {
+//					fS.close();
+//				}
+//				if (oS != null) {
+//					oS.close();
+//				}
+//			} catch (IOException e) {
+//				System.out.println(e.getMessage());
+//			}
+//		}
+//	}
 	
 
 }
